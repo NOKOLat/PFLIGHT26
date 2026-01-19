4d0fdf8fb45e762a3e550e92eb406fb6dc372181
aoi-netai <aoi256jp@Gmail.com>
2026-01-19 18:10:11 +0900

    loop_managerクラスをStateManagerから分離して、呼び出し用のコードを追加

---
+++ Core/Inc/loop_manager.hpp
@@ -0,0 +1,58 @@
+#ifndef LOOP_MANAGER_HPP
+#define LOOP_MANAGER_HPP
+
+#include "core_cm7.h"
+#include "stm32f7xx.h"
+
+class LoopManager {
+
+    public:
+
+        // コンストラクタ
+        LoopManager(unsigned long loop_interval_us = 100000) {
+
+            this->loop_interval_us = loop_interval_us;
+            this->last_cycle_count = 0;
+            
+            // DWT (Data Watchpoint and Trace) の初期化
+            CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+            DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+        }
+
+        // 次ループまで待機が必要かどうかを判定
+        bool isWaitNextLoop() {
+
+            uint32_t current_cycle_count = DWT->CYCCNT;
+
+            // 最初の実行時は待機しない
+            if (last_cycle_count == 0) {
+
+                last_cycle_count = current_cycle_count;
+                return false;
+            }
+
+            // サイクルカウントからマイクロ秒を計算
+            uint32_t delta_cycles = current_cycle_count - last_cycle_count;
+            unsigned long delta_time_us = delta_cycles / (SystemCoreClock / 1000000);
+
+            // 指定時間経過しているかの判定
+            if (delta_time_us >= loop_interval_us) {
+
+                last_cycle_count = current_cycle_count;
+                return false;
+            }
+
+            // 指定時間経過していない場合は待機
+            return true;
+        }
+
+    private:
+
+        // ループ間隔（マイクロ秒）
+        unsigned long loop_interval_us = 1000;
+
+        // 最後のDWTサイクルカウント
+        uint32_t last_cycle_count = 0;
+};
+
+#endif // LOOP_MANAGER_HPP
--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -1,13 +1,25 @@
 #include "wrapper.hpp"
 #include "tim.h"
+#include "optional.h"
 #include "stdio.h"
+
+#include "state_manager.hpp"
+#include "loop_manager.hpp"
+
+std::optional<StateManager*> state_manager;
+std::optional<LoopManager*> loop_manager;
 
 void init(){
 
-
+    loop_manager.emplace(20000); // 20ms
+    state_manager.emplace(StateID::INIT_STATE);
 }
 
 void loop(){
 
+    // 20msごとにStateManager::update()を呼び出す
+    if (!loop_manager->isWaitNextLoop()) {
 
+        state_manager->update();
+    }
 }
